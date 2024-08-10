6d1b93a8fb894d835d8c5741e579bf53a871e212
Vitalii-Karashchuk <83291711+Vitalii-Karashchuk@users.noreply.github.com>
2024-08-10 23:22:38 +0300

    save task3

---
+++ js/task-3.js
@@ -0,0 +1,26 @@
+const profile = {
+    username: "Jacob",
+  playTime: 300,
+  changeUsername(newName){
+    profile.username = newName;
+  },
+  updatePlayTime(hours){
+    profile.playTime = profile.playTime + hours;
+  },
+  getInfo(){
+    const Username = this.username;
+    const amount = this.playTime;
+    return `${Username} has ${amount} active hours!`
+  },
+};
+
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
